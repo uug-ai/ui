2417d01f1e7409c8102a37d4bae4a6bca182f2c0
Kilian Boute <kilian.boute@gmail.com>
2024-05-06 15:51:56 +0200

    Add long label button test case

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -4,6 +4,7 @@ export default {
   component: Button,
   title: "Button",
   tags: ["autodocs"],
+  argTypes: { handleClick: { action: "Handle click event" } },
 };
 
 const Template = (args) => <Button {...args} />;
@@ -13,4 +14,8 @@ export const Light = Template.bind({});
 Light.args = {
   backgroundColor: "blue-50",
   color: "blue-950",
+};
+export const LongLabel = Template.bind({});
+LongLabel.args = {
+  label: "This is a button with a longer label",
 };
--- stories/Button.tsx
+++ stories/Button.tsx
@@ -3,14 +3,17 @@ import React from "react";
 export default function Button({
   backgroundColor = "blue-950",
   color = "blue-50",
+  label = "Button",
+  handleClick,
 }) {
   return (
     <>
       <button
         type="button"
-        className={`bg-${backgroundColor} text-${color} w-36 h-10 rounded-lg text-lg focus:outline-none hover:cursor-pointer`}
+        className={`bg-${backgroundColor} text-${color} min-w-36 min-h-10 rounded-lg text-lg focus:outline-none hover:cursor-pointer`}
+        onClick={handleClick}
       >
-        Button
+        {label}
       </button>
     </>
   );
