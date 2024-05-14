cd50c6548d0cb06933d864d4302f35ce64357a94
Cedric Verstraeten <cedric@verstraeten.io>
2024-05-14 10:01:55 +0200

    Password field: add z-index and absolute postioning

--- components/Password/index.tsx
+++ components/Password/index.tsx
@@ -2,6 +2,8 @@ import { VariantProps, cva } from "class-variance-authority";
 import { ComponentProps, forwardRef, useState } from "react";
 import { Input } from "../Input";
 
+// Add style to show/hide button in abosolute position on the right
+// side of the input field.
 const PasswordStyles = cva([]);
 
 type PasswordProps = ComponentProps<"input"> &
@@ -16,9 +18,14 @@ export const Password = forwardRef<HTMLInputElement, PasswordProps>(
     const toggleVisibility = () => setVisible(!visible);
 
     return (
-      <div>
+      <div style={{ position: "relative" }}>
         <Input type={visible ? "text" : "password"} />{" "}
-        <span onClick={toggleVisibility}>{visible ? "Hide" : "Show"}</span>
+        <span
+          style={{ position: "absolute", right: "5%", top: "18%", zIndex: 999 }}
+          onClick={toggleVisibility}
+        >
+          {visible ? "Hide" : "Show"}
+        </span>
       </div>
     );
   }
