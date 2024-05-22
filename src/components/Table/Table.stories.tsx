146fd39cfbc514ef1b0a99485b1fb81f55af08b4
Kilian <kilian.boute@gmail.com>
2024-05-22 16:31:20 +0200

    Add Table component

---
+++ src/components/Table/Table.stories.tsx
@@ -0,0 +1,60 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Table, { ColumnProps } from "./Table";
+
+const meta: Meta<typeof Table> = {
+  title: "Layout/Table",
+  component: Table,
+};
+
+type Data = {
+  id: number;
+  name: string;
+  department: string;
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const data: Array<Data> = [
+  {
+    id: 1,
+    name: "John Doe",
+    department: "Engineering",
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    department: "Marketing",
+  },
+  {
+    id: 3,
+    name: "Mike Johnson",
+    department: "Sales",
+  },
+];
+const columns: Array<ColumnProps<Data>> = [
+  {
+    key: "id",
+    title: "Id",
+  },
+  {
+    key: "name",
+    title: "Name",
+  },
+  {
+    key: "department",
+    title: "Department",
+
+    render: (_, record) => {
+      return <div className="text-blue-500 font-bold">{record.department}</div>;
+    },
+  },
+];
+
+export const Default: Story = {
+  args: {
+    data,
+    columns,
+  },
+};
---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,75 @@
+import { cva } from "class-variance-authority";
+import { ReactElement } from "react";
+
+const TableStyles = cva(["table table-auto border-collapse w-full"], {
+  variants: {
+    size: {
+      sm: "text-sm",
+      md: "text-base",
+      lg: "text-lg",
+    },
+    colors: {
+      primary: "bg-primary-50 text-primary-950",
+      secondary: "bg-primary-950 text-primary-50",
+    },
+    defaultVariants: {
+      size: "md",
+      colors: "primary",
+    },
+  },
+});
+
+export interface ColumnProps<T> {
+  key: string;
+  title: string | ReactElement;
+  render?: (column: ColumnProps<T>, item: T) => ReactElement;
+}
+
+type Props<T> = {
+  columns: ColumnProps<T>[];
+  data?: T[];
+};
+
+const Table = <T,>({ data, columns }: Props<T>) => {
+  const headers = columns.map((column, index) => {
+    return (
+      <th key={`header-${index}`} className="">
+        {column.title}
+      </th>
+    );
+  });
+
+  const rows = !data?.length ? (
+    <tr>
+      <td colSpan={columns.length} className="">
+        No data
+      </td>
+    </tr>
+  ) : (
+    data?.map((row, index) => {
+      return (
+        <tr key={`row-${index}`}>
+          {columns.map((column, indexColumn) => {
+            const value = column.render
+              ? column.render(column, row as T)
+              : (row[column.key as keyof typeof row] as string);
+
+            return <td key={`cell-${indexColumn}`}>{value}</td>;
+          })}
+        </tr>
+      );
+    })
+  );
+
+  return (
+    <table className="">
+      <thead className="">
+        <tr>{headers}</tr>
+      </thead>
+
+      <tbody>{rows}</tbody>
+    </table>
+  );
+};
+
+export default Table;
