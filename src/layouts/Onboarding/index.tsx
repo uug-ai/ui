6f41589f09a05b75a00fe222cc29f89bc236cfa8
Luis Maja <146820631+LuisMaja@users.noreply.github.com>
2024-05-27 16:56:26 +0200

    update onboarding layout style

--- src/layouts/Onboarding/index.tsx
+++ src/layouts/Onboarding/index.tsx
@@ -2,19 +2,20 @@ import Box from "../../components/Box";
 import Button from "../../components/Button";
 import Gradient from "../../components/Gradient";
 import Input from "../../components/Input";
-import Logo from "../../components/Logo";
 import Row from "../../components/Row";
+import Socials from "../../components/Socials";
 import Stack from "../../components/Stack";
 import Text from "../../components/Text";
 import FaceScan from "../../components/icons/facescan";
 import Help from "../../components/icons/help";
+import Logo from "../../components/icons/logo";
 
 const Onboarding = () => {
   return (
     <Box className="bg-primary-50">
       <Stack>
         <Gradient />
-        <Row className="w-full justify-center pt-14 px-10">
+        <Row className="w-full pt-14 px-20">
           <FormSection />
           <FaceScanSection />
         </Row>
@@ -25,9 +26,9 @@ const Onboarding = () => {
 };
 
 const FormSection = () => (
-  <Stack className="w-1/2 flex items-center place-content-center">
+  <Stack className="w-1/3 flex ">
     <Box>
-      <Logo />
+      <Logo size={"lg"} className="pb-8" />
       <Box className="mb-4">
         <Text as="label" weight="semibold" className="mb-1">
           First Name
@@ -66,18 +67,18 @@ const FormSection = () => (
 );
 
 const FaceScanSection = () => (
-  <Stack className="w-1/2 h-fit flex items-center place-content-center">
+  <Stack className="w-2/3 h-fit flex items-center place-content-center">
     <Box>
       <FaceScan />
     </Box>
     <Box className="pt-10 w-full">
-      <Row className="justify-center">
-        <Box>
+      <Row>
+        <Row className="w-2/3">
           <Button type="submit" variant="solid" width="third">
             Scan Face
           </Button>
-        </Box>
-        <Box>
+        </Row>
+        <Box className="w-1/3">
           <Help />
         </Box>
       </Row>
@@ -86,7 +87,7 @@ const FaceScanSection = () => (
 );
 
 const InfoSection = () => (
-  <Stack className="p-14 text-center items-center place-content-center">
+  <Stack className="p-14 items-center place-content-center">
     <Box className="p-10 shadow-md rounded-md bg-white text-xl w-full ">
       <Text as="a" weight="bold" className="shadow-inner bg-white" />
       Hello, this is a registration from where you can register with a video of
@@ -94,28 +95,39 @@ const InfoSection = () => (
       We will use this video to create biometrics, so you get acces into the
       company and won't need to use a badge or card.
     </Box>
-    <Row className="pt-14">
+    <Row className="pt-14 space-x-10">
       <Box className="w-1/2">
-        <Text as="a" />
-        About UUG.Ai
+        <Stack>
+          <Text size={"lg"} weight={"bold"} className="pb-5">
+            About UUFT.Ai
+          </Text>
+          <Text as="a">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatibus vero reiciendis quae porro, voluptates odit. Culpa
+            ipsam beatae voluptas vitae est repudiandae, nulla atque, reiciendis
+            labore, voluptatibus eum dolorem! Id inventore quidem ipsam impedit
+            possimus?
+          </Text>
+        </Stack>
       </Box>
       <Box className="w-1/2">
-        <Text as="a" />
-        About UUG.Ai
+        <Stack>
+          <Text size={"lg"} weight={"bold"} className="pb-5">
+            About UUFT.Ai
+          </Text>
+          <Text as="a">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatibus vero reiciendis quae porro, voluptates odit. Culpa
+            ipsam beatae voluptas vitae est repudiandae, nulla atque, reiciendis
+            labore, voluptatibus eum dolorem! Id inventore quidem ipsam impedit
+            possimus?
+          </Text>
+        </Stack>
       </Box>
     </Row>
-    <Row className="text-sm text-center w-full space-x-10">
-      <Text as="a" />
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus vero
-      reiciendis quae porro, voluptates odit. Culpa ipsam beatae voluptas vitae
-      est repudiandae, nulla atque, reiciendis labore, voluptatibus eum dolorem!
-      Id inventore quidem ipsam impedit possimus?
-      <Text as="a" />
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia corrupti
-      aperiam, maiores nam nulla voluptatem enim suscipit, voluptatibus sunt,
-      deserunt blanditiis? Pariatur soluta, aliquid sunt, placeat officia quis
-      id libero temporibus nihil earum voluptas quasi!
-    </Row>
+    <Box>
+      <Socials size={"lg"} className="pt-16" />
+    </Box>
   </Stack>
 );
 
