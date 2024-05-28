eaaf04042071724a20f256fcc2a8b0905678b625
Luis Maja <146820631+LuisMaja@users.noreply.github.com>
2024-05-28 14:46:45 +0200

    add blank icon

---
+++ src/components/icons/blank.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+function Blank(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+    ></svg>
+  );
+}
+
+export default Blank;
