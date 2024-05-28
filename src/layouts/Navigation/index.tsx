37f59f32f86d1180c4ddfb2e2a628cf16199c7fc
Luis Maja <146820631+LuisMaja@users.noreply.github.com>
2024-05-28 14:34:56 +0200

    update navigation layout

--- src/layouts/Navigation/index.tsx
+++ src/layouts/Navigation/index.tsx
@@ -2,73 +2,64 @@ import React from "react";
 import Box from "../../components/Box";
 import Row from "../../components/Row";
 import Socials from "../../components/Socials";
-import Stack from "../../components/Stack";
 import Text from "../../components/Text";
-import IconDashboard from "../../components/icons/dashboard";
-import IconHistory from "../../components/icons/history";
-import IconLogOut from "../../components/icons/logout";
-import IconNotification from "../../components/icons/notification";
-import IconProfile from "../../components/icons/profile";
+import CameraSwap from "../../components/icons/cameraswap";
+import Dashboard from "../../components/icons/dashboard";
+import History from "../../components/icons/history";
+import Insights from "../../components/icons/insights";
+import LogOut from "../../components/icons/logout";
+import Notification from "../../components/icons/notification";
+import Profile from "../../components/icons/profile";
 
 const Navigation: React.FC = () => {
   return (
-    <Box className="border max-h-screen m-0 p-0">
-      <Stack>
-        <Box className="w-full h-screen">
-          <Box className="h-1/6 p-7 bg-slate-500">
-            <Text className="text-white text-lg weight-thin">UUFT.AI</Text>
-          </Box>
-          <Box className="h-3/5 bg-slate-100">
-            <a href="#" className="hover:bg-sky-700">
-              <Row className="pt-6">
-                <IconDashboard className="ml-5" />
-                <Text as="a" weight={"bold"} className="pl-2 pr-10">
-                  Dashboard
-                </Text>
-                <IconNotification className="mr-2 text-red-500" />
-              </Row>
-            </a>
+    <Box className="border m-0 p-0 h-screen flex flex-col">
+      <Box className="p-7 bg-slate-500 flex items-center justify-center">
+        <Text className="text-white text-lg weight-thin">UUFT.AI</Text>
+      </Box>
+      <Box className="flex-grow bg-slate-100 overflow-auto">
+        <Box>
+          <a href="#" className="hover:bg-sky-700 block">
             <Row className="pt-6">
-              <IconHistory className="ml-5" />
-              <Text as="a" weight={"bold"} className="pl-2 pr-10">
-                History
+              <Dashboard className="ml-5" />
+              <Text as="span" weight="bold" className="pl-2 pr-10">
+                Dashboard
               </Text>
+              <Notification className="mr-2 text-red-500" />
             </Row>
-            <Row className="pt-6">
-              <IconDashboard className="ml-5" />
-              <Text as="a" weight={"bold"} className="pl-2 pr-10">
-                Cameras
-              </Text>
-            </Row>
-            <Row className="pt-6">
-              <IconDashboard className="ml-5" />
-              <Text as="a" weight={"bold"} className="pl-2 pr-10">
-                Insights
-              </Text>
-            </Row>
-          </Box>
-          <Box className="bg-slate-100">
-            <Row className="pt-6">
-              <Socials className="pl-5 pb-5 gap-2 text-xs" />
-            </Row>
-          </Box>
-          <Box className="bg-slate-500 h-1/6">
-            <Stack>
-              <Row className="pt-6 place justify-between">
-                <IconProfile className="ml-5 text-white text-lg" />
-                <Text
-                  as="a"
-                  weight={"bold"}
-                  className="pl-2 pr-16 text-white text-lg"
-                >
-                  Profile
-                </Text>
-                <IconLogOut className="mr-2 text-white text-lg" />
-              </Row>
-            </Stack>
-          </Box>
+          </a>
+          <Row className="pt-6">
+            <History className="ml-5" />
+            <Text as="span" weight="bold" className="pl-2 pr-10">
+              History
+            </Text>
+          </Row>
+          <Row className="pt-6">
+            <CameraSwap className="ml-5" />
+            <Text as="span" weight="bold" className="pl-2 pr-10">
+              Cameras
+            </Text>
+          </Row>
+          <Row className="pt-6">
+            <Insights className="ml-5" />
+            <Text as="span" weight="bold" className="pl-2 pr-10">
+              Insights
+            </Text>
+          </Row>
         </Box>
-      </Stack>
+      </Box>
+      <Box className="bg-slate-100">
+        <Row className="pt-6">
+          <Socials className="pl-5 pb-5 gap-2 text-xs" />
+        </Row>
+      </Box>
+      <Box className="bg-slate-500 flex items-center flex flex-row w-full pt-6 pb-6">
+        <Profile className="text-white text-lg ml-5" />
+        <Text as="a" weight="bold" className="text-white text-lg pl-2 pr-10">
+          Profile
+        </Text>
+        <LogOut className="text-white text-lg ml-5" />
+      </Box>
     </Box>
   );
 };
