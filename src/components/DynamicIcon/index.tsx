fb71fdaca6a12665c08d67c99029b658d23f7477
Luis Maja <146820631+LuisMaja@users.noreply.github.com>
2024-05-28 14:47:04 +0200

    fix dynamic icon

--- src/components/DynamicIcon/index.tsx
+++ src/components/DynamicIcon/index.tsx
@@ -1,26 +1,25 @@
 import React, { Suspense } from "react";
 import Box from "../Box";
 import Row from "../Row";
-
-// This defines the structure of props that component expects to receive
+import Blank from "../icons/blank";
 
 interface IconProps {
   type: string;
 }
 
-// This dynamically imports an icon component using the (reacts) lazy function.
-// Meaning it will happen asynchronously, which can improve performance by splitting the code into smaller chunks.
-
 const Icon: React.FC<IconProps> = ({ type }) => {
-  const IconComponent = React.lazy(() => import(`../icons/${type}`));
+  // Use a fallback icon component in case the requested icon is not found
+  const FallbackIcon = Blank;
 
-  // The suspense component is used to handle the asynchornous loading of the icon component
+  const LazyIcon = React.lazy(() =>
+    import(`../icons/${type}`).catch(() => ({ default: FallbackIcon }))
+  );
 
   return (
     <Box className="Icon">
       <Row>
         <Suspense fallback={<div>Loading...</div>}>
-          <IconComponent />
+          <LazyIcon />
         </Suspense>
       </Row>
     </Box>
