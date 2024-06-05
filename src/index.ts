c00543303a63ae59145e6ab53b37d82b5f313bea
Luis Maja <146820631+LuisMaja@users.noreply.github.com>
2024-06-05 14:37:25 +0200

    Hotfix

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,17 @@ import Box from "./components/Box";
 import Button from "./components/Button";
 import Checkbox from "./components/Checkbox";
 import Dropdown from "./components/Dropdown";
+import Icon from "./components/DynamicIcon";
 import Gradient from "./components/Gradient";
 import Input from "./components/Input";
+import Logo from "./components/Logo";
 import Password from "./components/Password";
 import Row from "./components/Row";
 import Socials from "./components/Socials";
 import Stack from "./components/Stack";
 import Table from "./components/Table";
 import Text from "./components/Text";
+import VideoCapture from "./components/VideoCapture";
 
 export {
   Box,
@@ -17,11 +20,14 @@ export {
   Checkbox,
   Dropdown,
   Gradient,
+  Icon,
   Input,
+  Logo,
   Password,
   Row,
   Socials,
   Stack,
   Table,
   Text,
+  VideoCapture,
 };
