c5d96c266191fa582cd5463511e9efe0ad1d1bd6
Luis Maja <146820631+LuisMaja@users.noreply.github.com>
2024-06-20 15:21:25 +0200

    Create story and add dummydata for location

---
+++ src/layouts/Location/index.stories.tsx
@@ -0,0 +1,69 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Location } from ".";
+import { ColumnProps } from "../../components";
+
+const meta: Meta<typeof Location> = {
+  title: "Layout/Location",
+  component: Location,
+  parameters: {
+    layout: "fullscreen",
+  },
+};
+
+export default meta;
+
+type locationData = {
+  id: number;
+  name: string;
+};
+
+const locationData: Array<locationData> = [
+  {
+    id: 1,
+    name: "Location 1",
+  },
+  {
+    id: 2,
+    name: "Location 2",
+  },
+  {
+    id: 3,
+    name: "Location 3",
+  },
+  {
+    id: 4,
+    name: "Location 4",
+  },
+  {
+    id: 5,
+    name: "Location 5",
+  },
+  {
+    id: 6,
+    name: "Location 6",
+  },
+  {
+    id: 7,
+    name: "Location 7",
+  },
+  {
+    id: 8,
+    name: "Location 8",
+  },
+];
+
+const locationColumns: Array<ColumnProps<locationData>> = [
+  {
+    key: "name",
+    title: "Locations",
+  },
+];
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    locationColumns,
+    locationData,
+  },
+};
