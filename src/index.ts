ee02807e529e8cc2d67f28a5c475fa433fe51ae6
Luis Maja <146820631+LuisMaja@users.noreply.github.com>
2024-06-26 11:36:16 +0200

    Export all icons individually and increase version

--- src/index.ts
+++ src/index.ts
@@ -24,28 +24,62 @@ import Stack from "./components/Stack";
 import Table from "./components/Table";
 import Text from "./components/Text";
 import VideoCapture from "./components/VideoCapture";
+import IconLocationAdd from "./components/icons/addLocation";
+import IconUserAdd from "./components/icons/addUser";
+import Blank from "./components/icons/blank";
+import CameraSwap from "./components/icons/cameraswap";
+import Dashboard from "./components/icons/dashboard";
+import FaceScan from "./components/icons/facescan";
+import Github from "./components/icons/github";
+import Help from "./components/icons/help";
+import History from "./components/icons/history";
+import Insights from "./components/icons/insights";
+import Linkedin from "./components/icons/linkedin";
+import logo from "./components/icons/logo";
+import LogOut from "./components/icons/logout";
+import Notification from "./components/icons/notification";
+import Profile from "./components/icons/profile";
+import Slack from "./components/icons/slack";
+import Youtube from "./components/icons/youtube";
 
 export {
+  Blank,
   Box,
   Button,
+  CameraSwap,
   Checkbox,
+  Dashboard,
   Dropdown,
   DynamicIcon,
+  FaceScan,
+  Github,
   Gradient,
+  Help,
+  History,
   Icon,
+  IconLocationAdd,
+  IconUserAdd,
   Input,
+  Insights,
   KPI,
+  Linkedin,
+  LogOut,
   Logo,
   Maps,
   NavItem,
   NavigationFooter,
   NavigationHeader,
   NavigationSocials,
+  Notification,
   Password,
+  Profile,
   Row,
+  Slack,
   Socials,
   Stack,
   Table,
   Text,
   VideoCapture,
+  Youtube,
+  logo,
 };
